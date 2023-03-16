564ae1cd25ec50aaa71cc0abb6b793335bf951a3
Drew Kennelly <drewkennelly@gmail.com>
2023-03-17 11:17:44 +1300

    add handRank test

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,13 +1,29 @@
 import { handRank, compareHands, parseHand, Hand } from "../index";
 
-describe("parseHand", () => {
-  
-});
+describe("parseHand", () => {});
 
 describe("handRank", () => {
-  
+  it("calculates the rank of a hand", () => {
+    const input: Hand = [
+      [0, "D"],
+      [1, "C"],
+      [2, "S"],
+      [3, "H"],
+      [4, "C"],
+    ];
+    const expected = {
+      rank: 5,
+      cards: [
+        [4, ""],
+        [3, ""],
+        [2, ""],
+        [1, ""],
+        [0, ""],
+      ],
+    };
+
+    expect(handRank(input)).toEqual(expected);
+  });
 });
 
-describe("compareHands", () => {
-  
-});
+describe("compareHands", () => {});
